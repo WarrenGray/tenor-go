f73be151a347c12da988fb7d99ff011b816f14c6
Warren Gray <warren.gray@arcticwolf.com>
2021-02-24 20:12:39 -0500

    Fix package name.

--- generate.sh
+++ generate.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -ex
-docker run --rm -v "${PWD}":/src openapitools/openapi-generator-cli generate -i /src/openapi.yaml -o /src -g go --git-repo-id=tenor-go --git-user-id=warrengray
+docker run --rm -v "${PWD}":/src openapitools/openapi-generator-cli generate \
+  -i /src/openapi.yaml \
+  -o /src \
+  -g go \
+  --git-repo-id=tenor-go \
+  --git-user-id=warrengray \
+  --package-name tenor
 
 # remove unnecessary files
 rm -rf api
